f52d6605fb61c351768bb3f56d28f380091a8345
DanielYaoPeng <down953@163.com>
2020-02-01 15:09:49 +0800

    localcache

--- src/App.vue
+++ src/App.vue
@@ -8,24 +8,29 @@
     <h2>进行中</h2>
     <ul>
       <li v-for="(item,key) in listOne" v-if="!item.checked">
-        <input type="checkbox" v-model="item.checked" />
+        <input type="checkbox" v-model="item.checked" @change="saveList()" />
         {{item.title}} ----
         <button @click="removeDO(key)">删除</button>
       </li>
     </ul>
 
     <h2>已完成</h2>
-    <ul class="finish">
+    <ul class="finish"> 
       <li v-for="(item,key) in listOne" v-if="item.checked">
-        <input type="checkbox" v-model="item.checked" />
+        <input type="checkbox" v-model="item.checked" @change="saveList()"/>
         {{item.title}} ----
         <button @click="removeDO(key)">删除</button>
       </li>
     </ul>
+
+    <h2>获取渲染对象的数据</h2>
+    <button @click="getItem()">点击获取数据</button>
   </div>
 </template>
 
 <script>
+import cache from './model/cache.js';
+var dataKey='key';
 export default {
   name: "app",
   data() {
@@ -33,11 +38,11 @@ export default {
       todomsg: "",
       listOne: [
         {
-          title: "one",
+          title: "录制nodejs",
           checked: false
         },
         {
-          title: "two",
+          title: "录制vue",
           checked: true
         }
       ]
@@ -50,10 +55,26 @@ export default {
       if (e.keyCode == 13 && this.todomsg != "") {
         this.listOne.push({ title: this.todomsg, checked: false });
         this.todomsg = "";
+        cache.set(dataKey,this.listOne);
       }
     },
     removeDO(key) {
       this.listOne.splice(key, 1);
+      cache.set(dataKey,this.listOne);
+    },
+    getItem() {
+      console.log(this.listOne);
+    },
+    saveList(){
+       cache.set(dataKey,this.listOne);
+    }
+  },
+  mounted: function() {
+    //程序启动的时候初始化list
+
+    let initDs = cache.get(dataKey);
+    if (initDs != undefined&&initDs) {
+      this.listOne = initDs;
     }
   }
 };
